b272ad71b5a5a6e8ff0acbefe368a109bdc3b75c
Preet Biswas <preetbiswas2006@gmail.com>
2025-09-09 21:45:41 +0530

    Update about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,7 @@ const About = () => {
       <div className="about-me">
         <h3 className="title">About Me</h3>
         <p className="para">
-        I’m a passionate full-stack engineer building smart, scalable systems that anticipate tomorrow’s needs. With a deep grasp of modern tech and clean design, I move fast, solve hard problems, and deliver with precision. I don’t just write code — I build momentum.
+        I’m a passionate full-stack developer and software engineer building smart, scalable systems that anticipate tomorrow’s needs. With a deep grasp of modern tech and clean design, I move fast, solve hard problems, and deliver with precision. I don’t just write code — I build momentum.
         </p>
       </div>
     </div>
